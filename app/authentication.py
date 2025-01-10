afd9cee7c8b4aa7b9feed2983e22d28cea587ac2
femarko <fenstermark7@gmail.com>
2025-01-11 00:37:48 +0100

    authentication.check_current_user() modified

--- app/authentication.py
+++ app/authentication.py
@@ -1,17 +1,18 @@
+from dataclasses import dataclass
 from typing import Any
-
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity
 
 from app import adv
-from app.error_handlers import HttpError
-from app.models import UserColumns, AdvertisementColumns
-from app.base_resut import BaseResult
+from app.models import UserColumns
+
 
 jwt = JWTManager(app=adv)
 
 
-class CurrentUserCheckFailedError(Exception):
-    pass
+@dataclass
+class CurrentUserResult:
+    result: int | None = None
+    errors: bool = False
 
 
 def get_access_token(identity: UserColumns) -> str:
@@ -36,28 +37,8 @@ def get_authenticated_user_identity() -> Any:
     return get_jwt_identity()
 
 
-# def check_current_user(current_user_id: int, user_id: int) -> bool:
-#     """
-#     The function checks, whether an integer, passed as "user_id" parameter equals to the authenticated (current)
-#     user's ID.
-#
-#     :param current_user_id: authenticated user's ID
-#     :type current_user_id: int
-#     :param user_id: an integer, which is a subject to check
-#     :type user_id: int
-#     :return: True | False
-#     :rtype: bool
-#     """
-#     if user_id == current_user_id:
-#         return True
-#     return False
-
-
-def check_current_user(user_id: int | None = None, get_cuid: bool = True) -> int | None:
+def check_current_user(user_id: int | None = None, get_cuid: bool = True) -> CurrentUserResult:
     current_user_id: int = get_jwt_identity()
     if user_id is None or user_id == current_user_id:
-        if get_cuid is True:
-            return current_user_id
-        else:
-            return
-    raise CurrentUserCheckFailedError
+        return CurrentUserResult(result=current_user_id)
+    return CurrentUserResult(errors=True)
