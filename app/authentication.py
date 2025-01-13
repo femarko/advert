eeb314c803bd6ddf233b027eb6771e0667076bdd
femarko <fenstermark7@gmail.com>
2025-01-13 15:51:05 +0100

    authentication.check_current_user: CurrentUserResult removed, app.errors.CurrentUserError implemented.

--- app/authentication.py
+++ app/authentication.py
@@ -2,17 +2,12 @@ from dataclasses import dataclass
 from typing import Any
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity
 
+import app.errors
 from app import adv
 from app.models import UserColumns
 
 
 jwt = JWTManager(app=adv)
-
-
-@dataclass
-class CurrentUserResult:
-    result: int | None = None
-    errors: bool = False
 
 
 def get_access_token(identity: UserColumns) -> str:
@@ -37,8 +32,10 @@ def get_authenticated_user_identity() -> Any:
     return get_jwt_identity()
 
 
-def check_current_user(user_id: int | None = None, get_cuid: bool = True) -> CurrentUserResult:
+def check_current_user(user_id: int | None = None, get_cuid: bool = True) -> int | None:
     current_user_id: int = get_jwt_identity()
     if user_id is None or user_id == current_user_id:
-        return CurrentUserResult(result=current_user_id)
-    return CurrentUserResult(errors=True)
+        if get_cuid is False:
+            return
+        return current_user_id
+    raise app.errors.CurrentUserError
