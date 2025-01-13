d0fad68373035844b4f06bcdd3ad427cf4a9d71c
femarko <fenstermark7@gmail.com>
2025-01-13 22:10:22 +0100

    service_layer.update_user() added.

--- app/service_layer.py
+++ app/service_layer.py
@@ -62,6 +62,23 @@ def create_user(user_data: dict[str, str], uow):
         return created_user_id
 
 
+def update_user(authenticated_user_id: int,
+                check_current_user_func: Callable,
+                validate_func: Callable,
+                hash_pass_func: Callable,
+                new_data: dict[str, str],
+                uow):
+    check_current_user_func(user_id=authenticated_user_id, get_cuid=False)
+    validated_data: dict[str, str] = validate_func(**new_data)
+    if validated_data["password"]:
+        validated_data["password"] = hash_pass_func(password=validated_data["password"])
+    with uow:
+        user_to_update: User = uow.users.get(authenticated_user_id)
+        for attr_name, attr_value in new_data.items():
+            setattr(user_to_update, attr_name, attr_value)
+        uow.add(user_to_update)
+
+
 def get_related_advs(current_user_id: int, page: int, per_page: int, uow) -> dict[str, int | list[ModelClass]]:
     # with uow:
     #     filter_result = uow.advs.get_paginated(filter_type=FilterTypes.COLUMN_VALUE,
