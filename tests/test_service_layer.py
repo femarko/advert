5d92219431f00cc3b28c5fb66240ded3028ab7e9
femarko <fenstermark7@gmail.com>
2025-01-16 20:28:32 +0100

    tests.test_service_layer.test_create_user added and passed.

--- tests/test_service_layer.py
+++ tests/test_service_layer.py
@@ -45,6 +45,27 @@ def test_get_user_data(fake_check_current_user_func, fake_unit_of_work, fake_use
     assert result == expected_result
 
 
+def test_create_user(
+        fake_validate_func, fake_hash_pass_func, fake_users_repo, fake_advs_repo, fake_unit_of_work, test_date
+):
+    user_data = {
+        "name": "test_name", "email": "test@email.test", "password": "test_password", "creation_date": test_date
+    }
+    fuow = fake_unit_of_work(users=fake_users_repo([]), advs=fake_advs_repo([]))
+    result = service_layer.create_user(
+        user_data=user_data, validate_func=fake_validate_func, hash_pass_func=fake_hash_pass_func,
+        uow=fuow
+    )
+    data_from_repo = next(item for item in fuow.users.instances)
+    assert type(result) is int
+    assert 0 <= result <= 9
+    assert result == data_from_repo.id
+    assert data_from_repo.name == user_data["name"]
+    assert data_from_repo.email == user_data["email"]
+    assert data_from_repo.password == user_data["password"]
+    assert data_from_repo.creation_date == user_data["creation_date"]
+
+
 def test_update_user(
         fake_check_current_user_func, fake_validate_func, fake_hash_pass_func, fake_users_repo, fake_advs_repo,
         fake_unit_of_work
