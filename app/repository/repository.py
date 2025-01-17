aaa176bf62e55c3cf9003cb26aebb94772bc55e4
femarko <fenstermark7@gmail.com>
2025-01-17 03:29:30 +0100

    In app.repository.repository.get_list_or_paginated_data(): filter_func param remoed from signature.

--- app/repository/repository.py
+++ app/repository/repository.py
@@ -1,17 +1,17 @@
 from dataclasses import dataclass
-from typing import Callable, Any, Protocol
+from typing import Callable, Any, Protocol, Optional
 
 from sqlalchemy.exc import IntegrityError
 
 import app.errors
 import app.service_layer
-from app.models import User, Advertisement, ModelClass
+from app.models import User, Advertisement, ModelClass, UserColumns, AdvertisementColumns
+from app.repository import filtering
+from app.repository.filtering import FilterTypes, Comparison
 
 
 class NotFoundError(Exception):
     pass
-
-
 
 
 class RepoProto(Protocol):
@@ -42,8 +42,24 @@ class Repository:
     def get(self, instance_id):
         return self.session.get(self.model_cl, instance_id)
 
-    def get_list_or_paginated_data(self, filter_func: Callable, **kwargs: Any):
-        return filter_func(self.session, model_class=self.model_cl, **kwargs)
+    def get_list_or_paginated_data(self, filter_type: FilterTypes,
+                                   comparison: Comparison,
+                                   column: UserColumns | AdvertisementColumns,
+                                   column_value,
+                                   paginate: Optional[bool] = False,
+                                   page: Optional[int] = None,
+                                   per_page: Optional[int] = None):
+        return filtering.get_list_or_paginated_data(
+            session=self.session,
+            model_class=self.model_cl,
+            filter_type=filter_type,
+            comparison=comparison,
+            column=column,
+            column_value=column_value,
+            paginate=paginate,
+            page=page,
+            per_page=per_page
+        )
 
     def delete(self, instance):
         self.session.delete(instance)
