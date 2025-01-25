cc42a9a9bfc8e88d23d29094503c2b30698c0541
femarko <fenstermark7@gmail.com>
2025-01-25 01:04:48 +0100

    test_views.py::test_update_adv_returns_401_when_user_is_unauthorized added and passed.

--- tests/test_views.py
+++ tests/test_views.py
@@ -506,6 +506,13 @@ def test_update_adv_returns_403_when_current_user_check_fails(
     assert response.json == {"errors": "Unavailable operation."}
 
 
+def test_update_adv_returns_401_when_user_is_unauthorized(
+        clear_db_before_and_after_test, test_client, create_adv_through_http, test_adv_id
+):
+    new_adv_params = {"title": "new_title"}
+    response = test_client.patch(f"http://127.0.0.1:5000/advertisements/{test_adv_id}/", json=new_adv_params)
+    assert response.status_code == 401
+    assert response.json == {'msg': 'Missing Authorization Header'}
 
 
 
