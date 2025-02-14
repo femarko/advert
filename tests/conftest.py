4119ebcb118757f6c256fa65a4c2714a721cf58b
femarko <fenstermark7@gmail.com>
2025-02-14 17:38:17 +0100

    tests/conftest.py modified; 162 tests passed.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import datetime
-import random
-from typing import Optional, Literal
+from typing import Optional
 
 import pytest
 import sqlalchemy
@@ -19,21 +18,11 @@ def session_maker(engine):
     return sqlalchemy.orm.sessionmaker(bind=engine)
 
 
-@pytest.fixture(scope="session")
-def drop_all_create_all(engine):
-    models.Base.metadata.drop_all(bind=engine)
-    models.Base.metadata.create_all(bind=engine)
-
-
 @pytest.fixture
 def clear_db_before_and_after_test(engine):
     table_mapper.mapper.metadata.drop_all(bind=engine)
     table_mapper.mapper.metadata.create_all(bind=engine)
-    # models.Base.metadata.drop_all(bind=engine)
-    # models.Base.metadata.create_all(bind=engine)
     yield
-    # models.Base.metadata.drop_all(bind=engine)
-    # models.Base.metadata.create_all(bind=engine)
     table_mapper.mapper.metadata.drop_all(bind=engine)
     table_mapper.mapper.metadata.create_all(bind=engine)
 
@@ -49,49 +38,9 @@ def app_context():
     return adv.app_context()
 
 
-# @pytest.fixture
-# def access_token(session_maker, app_context, test_client, create_test_users_and_advs) -> dict[str, str]:
-#     access_token_dict = {}
-#     for i in range(1000, 1002):
-#         login_response = test_client.post(
-#             "http://127.0.0.1:5000/login/", json={"email": f"test_filter_{i}@email.com",
-#                                                   "password": f"test_filter_{i}_pass"}
-#         )
-#         login_response_json = login_response.json
-#         access_token_dict[f"user_{i}"] = login_response_json.get("access_token")
-#     return access_token_dict
-
-
 @pytest.fixture
 def test_date():
     return datetime.datetime(1900, 1, 1)
-
-
-# @pytest.fixture
-# def create_test_date_users(session_maker, test_date):
-#     session = session_maker
-#     with session() as sess:
-#         for i in range(1000, 1002):
-#             user = {"id": i,
-#                     "name": f"test_filter_{i}",
-#                     "email": f"test_filter_{i}@email.com",
-#                     "password": f"test_filter_{i}_pass"}
-#             sess.execute(sqlalchemy.text('INSERT INTO "user" (id, name, email, password, creation_date) '
-#                                          'VALUES (:id, :name, :email, :password, :creation_date)'),
-#                          dict(id=user["id"],
-#                               name=user["name"],
-#                               email=user["email"],
-#                               password=user["email"],
-#                               creation_date=test_date))
-#             sess.commit()
-#
-#     yield
-#
-#     with session() as sess:
-#         for i in range(1, 3):
-#             sess.execute(sqlalchemy.text('DELETE FROM "user" WHERE (creation_date = :test_creation_date)'),
-#                          dict(test_creation_date=test_date))
-#             sess.commit()
 
 
 @pytest.fixture
@@ -261,32 +210,6 @@ class FakeUnitOfWork:
 
 
 @pytest.fixture
-def test_user(test_date):
-    user = models.User(
-        id=1,
-        name="test_name",
-        email="test_email",
-        password="test_pass",
-        creation_date=test_date,
-        advertisements=[]
-    )
-    return user
-
-
-@pytest.fixture
-def test_adv(test_date, test_user):
-    testadv = models.Advertisement(
-        id=2,
-        title="test_adv",
-        description="test_description",
-        creation_date=test_date,
-        user_id=test_user.id,
-        user=None
-    )
-    return testadv
-
-
-@pytest.fixture
 def test_user_data():
     return {"name": "test_name", "email": "test@email.test", "password": "test_pass"}
 
@@ -294,8 +217,3 @@ def test_user_data():
 @pytest.fixture
 def test_adv_params(test_user_data):
     return {"title": "test_title", "description": "test_description", "user_id": 1}
-
-
-@pytest.fixture(scope="session")
-def start_mapping():
-    table_mapper.start_mapping()
